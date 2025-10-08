a51b6c54bbb5a7cdeb0fd63acf277c77c81d43da
IraChekh <iryna.chekh@nure.ua>
2025-10-08 15:56:35 +0300

    create a burger menu

--- Menu.js
+++ Menu.js
@@ -1,16 +1,30 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    function openMenu(menu, showMenu){
+     menu.style.right='0';
+     showMenu.style.opacity='0';
+    }
+
+    function closeMenu(menu,showMenu){
+        menu.style.right='-350px';
+        showMenu.style.opacity='1';
+    }
+
     const showMenu= document.querySelector(".show_menu");
     const hideMenu= document.querySelector(".close_menu");
     const menu = document.querySelector(".hidden_menu");
-    const links=document.querySelector(".link")
+    const links=document.querySelectorAll(".link")
+    
     showMenu.addEventListener("click",()=>{
-        menu.style.right='0';
-        showMenu.style.opacity='0';
-        
+        openMenu(menu,showMenu)
     })
 
-    hideMenu.addEventListener("click",()=>{
-        menu.style.right='-350px';
-        showMenu.style.opacity='1';
+    hideMenu.addEventListener("click", ()=>{
+        closeMenu(menu,showMenu)
     })
+    links.forEach(element => {
+        element.addEventListener("click",()=>{
+            closeMenu(menu,showMenu)
+        })
+        
+    });
 })
