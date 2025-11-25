b5be0897dfdb4bca879136e0cc74954c9e3c522d
IraChekh <iryna.chekh@nure.ua>
2025-11-25 14:18:29 +0200

    Add a pop-up modal window

--- checkInput.js
+++ checkInput.js
@@ -20,51 +20,54 @@ function check_input() {
 }
 
 
-function check_comment(comment,mistake){
-     if (comment === "") { 
-        mistake.textContent = "Please enter your comment";
-        return false
-    }
-    mistake.textContent =""
-    return true
+function check_comment(comment, mistake) {
+  mistake.style = 'visibility:visible'
+  if (comment === "") {
+    mistake.textContent = "Please enter your comment";
+    return false
+  }
+  mistake.style = 'visibility: hidden'
+  return true
 }
 
-function check_email(email,mistake){
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
- 
-    if (email === "") { 
-        mistake.textContent = "Please enter your email"; 
-        return false
-    } else if (!emailRegex.test(email)) { 
-       mistake.textContent = "Please enter a valid email";
-       return false
-    }
-    mistake.textContent= ""
-    return true
+function check_email(email, mistake) {
+  mistake.style = 'visibility:visible'
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email === "") {
+    mistake.textContent = "Please enter your email";
+    return false
+  } else if (!emailRegex.test(email)) {
+    mistake.textContent = "Please enter a valid email";
+    return false
+  }
+  mistake.style = 'visibility: hidden'
+  return true
 }
 
-function check_phone(phone,mistake){
-    const phoneRegex = /^(\+38)?0[0-9]{9}$/;
-    if (phone === "") { 
-        mistake.textContent = "Please enter your phone";
-        return false
-    } else if (!phoneRegex.test(phone)) { 
-        mistake.textContent = "Please enter a valid phone";
-        return false
-    }
-    mistake.textContent= ""
-    return true
+function check_phone(phone, mistake) {
+  mistake.style='visibility:visible'
+  const phoneRegex = /^(\+38)?0[0-9]{9}$/;
+  if (phone === "") {
+    mistake.textContent = "Please enter your phone";
+    return false
+  } else if (!phoneRegex.test(phone)) {
+    mistake.textContent = "Please enter a valid phone";
+    return false
+  }
+  mistake.style = 'visibility: hidden'
+  return true
 }
 
-function check_name(name, mistake){
-    const nameRegex = /^[A-Za-z]+$/;
-    if (name === "") {
-        mistake.textContent = "Please enter your name/surname";
-        return false
-    } else if (!nameRegex.test(name)) { 
-        mistake.textContent = "Please enter valid name/surname";
-        return false
-    }
-    mistake.textContent= ""
-    return true
+function check_name(name, mistake) {
+  mistake.style='visibility:visible'
+  const nameRegex = /^[A-Za-z]+$/;
+  if (name === "") {
+    mistake.textContent = "Please enter your name/surname";
+    return false
+  } else if (!nameRegex.test(name)) {
+    mistake.textContent = "Please enter valid name/surname";
+    return false
+  }
+  mistake.style = 'visibility: hidden'
+  return true
 }
---
+++ modal_window.js
@@ -0,0 +1,38 @@
+const TIME = 30000
+
+let open_button = document.getElementById("open_button");
+let modal = document.getElementById("modal");
+let body = document.getElementsByTagName('body')[0];
+let close_button = document.getElementById("close_modal");
+
+
+open_button.onclick = open_modal;
+
+close_button.onclick = close_modal;
+
+function open_modal() {
+    modal.classList.add('open_modal');
+    body.classList.add('body_block');
+    open_button.classList.add('close_modal');
+}
+
+function close_modal() {
+    modal.classList.remove('open_modal');
+    body.classList.remove('body_block');
+    open_button.classList.remove('close_modal');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(open_modal, TIME);
+});
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') close_modal();
+});
+
+document.addEventListener('click', (e) => {
+    if (modal.classList.contains('open_modal')) {
+        if (e.target != modal && e.target !=open_button) {
+            close_modal();
+        }
+    }
+})
